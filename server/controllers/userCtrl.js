edefc6338b431c538cd4bd5593fbd3c446d0550e
Mayank <mayanksingh12@gmail.com>
2024-06-09 17:25:13 +0530

    first commit

---
+++ server/controllers/userCtrl.js
@@ -0,0 +1,81 @@
+const Users = require("../models/userModel");
+
+const jwt = require("jsonwebtoken");
+
+const bcrypt = require("bcrypt");
+
+const userCtrl = {
+  register: async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+      console.log(Users);
+      const user = await Users.findOne({ email });
+      if (user)
+        return res.status(400).json({ msg: "The email already exists." });
+
+      if (password.length < 6)
+        return res
+          .status(400)
+          .json({ msg: "Password is at least 6 characters long." });
+
+      // Password Encryption
+      const passwordHash = await bcrypt.hash(password, 10);
+
+      const newUser = new Users({
+        name,
+        email,
+        password: passwordHash,
+      });
+
+      // Save mongodb
+      await newUser.save();
+
+      //create jwt to atuthenticate
+      const accesstoken = createAccessToken({ id: newUser._id });
+      const refreshToken = createRefreshToken({ id: newUser._id });
+
+      res.cookie("refreshtoken", refreshToken, {
+        httpOnly: true,
+        path: "/user/refresh_token",
+      });
+
+      res.json({ accesstoken });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  refreshToken: async (req, res) => {
+    try {
+      const rf_token = req.cookies.refreshtoken;
+
+      if (!rf_token)
+        return res.status(400).json({ msg: "Please Login or Register" });
+
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err)
+          return res.status(400).json({ msg: "Please Login or Register" });
+
+        const accesstoken = createAccessToken({ id: user.id });
+        res.json({ user, accesstoken });
+      });
+
+
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+const createAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+};
+
+const createRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+module.exports = userCtrl;
---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Ecommerce Server' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+//Routes
+app.use('/user', require('./routes/userRouter'));
+
+// Connect MongoDB
+const URI = process.env.MONGODB_URL;
+
+mongoose.connect(URI, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(error => {console.log(error)});
