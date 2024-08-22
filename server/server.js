0d9f9df9c0c9dfb6398a5f5de8350f2a87f4239b
Mayank <mayanksingh122001@gmail.com>
2024-08-22 18:09:35 +0530

    chore: Add CORS middleware for cross-origin requests

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
+const cors = require('cors');
 
 const app = express();
 app.use(express.json())
@@ -10,6 +11,12 @@ app.use(cookieParser())
 app.use(fileUpload({
     useTempFiles: true
 }))
+
+app.use(cors({
+    origin: ["https://zynka.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 
 const PORT = process.env.PORT || 5000;
 
